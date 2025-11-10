ef0df088d548a148f63cb3c1737e3151348f4b77
gaurang1404 <rshirodkar717@gmail.com>
2025-11-10 19:42:52 +0530

    width updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-white text-slate-900">
       <Navbar />
-      <main className="container 2xl:px-0 px-4 py-10">
+      <main className="container 2xl:px-0 px-4 py-10 max-w-[1200px] m-auto">
         <div className="max-w-[1280px] mx-auto">
           <HomePage />
         </div>
